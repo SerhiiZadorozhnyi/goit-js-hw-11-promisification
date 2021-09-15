cb2b26b73374970594f6dc9aefa1901fa38fdded
SerhiiZadorozhnyi <71183966+SerhiiZadorozhnyi@users.noreply.github.com>
2021-09-15 20:57:03 +0300

    Fix and clear code

--- js/task-02.js
+++ js/task-02.js
@@ -1,19 +1,19 @@
 const users = [
-    { name: 'Mango', active: true },
-    { name: 'Poly', active: false },
-    { name: 'Ajax', active: true },
-    { name: 'Lux', active: false },
+  { name: 'Mango', active: true },
+  { name: 'Poly', active: false },
+  { name: 'Ajax', active: true },
+  { name: 'Lux', active: false },
 ];
   
-  const toggleUserState = (allUsers, userName) => {
-    const updatedUsers = allUsers.map(user =>
-        user.name === userName ? {...user, active: !user.active } : user,
-    );
+const toggleUserState = (allUsers, userName) => {
+  const updatedUsers = allUsers.map(user =>
+    user.name === userName ? {...user, active: !user.active } : user,
+  );
+
+  return Promise.resolve(updatedUsers);
+};
   
-    return Promise.resolve(updatedUsers);
-  };
-   
-  const logger = updatedUsers => console.table(updatedUsers);
-  
-  toggleUserState(users, 'Mango').then(logger);
-  toggleUserState(users, 'Lux').then(logger);
+const logger = updatedUsers => console.table(updatedUsers);
+
+toggleUserState(users, 'Mango').then(logger);
+toggleUserState(users, 'Lux').then(logger);
